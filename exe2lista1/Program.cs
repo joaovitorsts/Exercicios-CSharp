7020175678130ed070e601a195f22b2e289bff04
João Vitor dos Santos <57101445+joaovitorsts@users.noreply.github.com>
2020-04-06 17:40:26 -0300

    Add files via upload

---
+++ exe2lista1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi = 3.14159;
+            Console.WriteLine("Digite o valor do raio da circunferência");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double area = pi * Math.Pow(2, raio);
+            Console.WriteLine($"A área é igual a {area.ToString("F4", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
