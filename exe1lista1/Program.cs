1d8dd367550c75b46c7077be5f671c135be626c0
João Vitor dos Santos <57101445+joaovitorsts@users.noreply.github.com>
2020-04-06 16:20:41 -0300

    Add files via upload

---
+++ exe1lista1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número inteiro");
+            int n1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite outro número inteiro");
+            int n2 = int.Parse(Console.ReadLine());
+
+            int soma = n1 + n2;
+            Console.WriteLine($"A soma dos valores é igual a {soma}");
+        }
+    }
+}
