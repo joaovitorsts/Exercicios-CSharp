c879898a39eb10bfa391c7705a047a6748555d63
João Vitor dos Santos <57101445+joaovitorsts@users.noreply.github.com>
2020-04-06 17:58:23 -0300

    Add files via upload

---
+++ exe3lista1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int n3 = int.Parse(Console.ReadLine());
+            int n4 = int.Parse(Console.ReadLine());
+
+            int res = n1 * n2 - n3 * n4;
+            Console.WriteLine($"DIFERENÇA = {res}");
+        }
+    }
+}
