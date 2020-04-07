2af18ca467e9c82130b2a13a932215dd45ea9820
João Vitor dos Santos <57101445+joaovitorsts@users.noreply.github.com>
2020-04-07 13:46:59 -0300

    Add files via upload

---
+++ exe4lista1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero_funcionario = int.Parse(Console.ReadLine());
+            int horas_trabalhadas = int.Parse(Console.ReadLine());
+            float valor_hora = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            float salario = valor_hora * horas_trabalhadas;
+            Console.WriteLine($"NUMBER = {numero_funcionario}");
+            Console.WriteLine($"SALARY = {salario.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
