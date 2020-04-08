cffc8b83498c8151fb6037ca4989cfa4bc5a9b73
João Vitor dos Santos <57101445+joaovitorsts@users.noreply.github.com>
2020-04-08 16:49:07 -0300

    Add files via upload

---
+++ exe2lista2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um valor inteiro");
+            int n1 = int.Parse(Console.ReadLine());
+            if(n1 % 2 == 0)
+                Console.WriteLine("PAR");
+            else
+                Console.WriteLine("ÍMPAR");
+        }
+    }
+}
