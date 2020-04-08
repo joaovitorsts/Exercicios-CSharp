230c81f74a1b70b138cf78c32fe3de719b53493e
João Vitor dos Santos <57101445+joaovitorsts@users.noreply.github.com>
2020-04-08 16:42:57 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um valor inteiro");
+            int n1 = int.Parse(Console.ReadLine());
+            if(n1 < 0)
+                Console.WriteLine("NEGATIVO");
+            else
+                Console.WriteLine("NÃO NEGATIVO");
+        }
+    }
+}
