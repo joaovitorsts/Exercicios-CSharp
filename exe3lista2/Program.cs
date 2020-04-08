dfb3241b5eb0d19923c7b35536ac52e8b945ca68
João Vitor dos Santos <57101445+joaovitorsts@users.noreply.github.com>
2020-04-08 17:05:12 -0300

    Add files via upload

---
+++ exe3lista2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um valor inteiro");
+            int n1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite outro valor inteiro");
+            int n2 = int.Parse(Console.ReadLine());
+
+            bool is_divisible = true;
+
+            if (n1 > n2)
+                is_divisible = n1 % n2 == 0;
+            else
+                is_divisible = n2 % n1 == 0;
+
+            if (is_divisible)
+                Console.WriteLine("São múltiplos");
+            else
+                Console.WriteLine("Não são múltiplos");
+
+        }
+    }
+}
