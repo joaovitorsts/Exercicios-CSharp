06056a6f0ad1e46f5224542af0f0ef0f950902c5
João Vitor dos Santos <57101445+joaovitorsts@users.noreply.github.com>
2020-04-16 15:11:21 -0300

    Add files via upload

---
+++ lista3exe1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Digite a senha");
+            string senha = Console.ReadLine();
+            while (senha != "2002")
+            {
+                Console.WriteLine("Senha Inválida");
+                senha = Console.ReadLine();
+            }
+
+            Console.WriteLine("Acesso Permitido");
+
+        }
+
+    }
+}
