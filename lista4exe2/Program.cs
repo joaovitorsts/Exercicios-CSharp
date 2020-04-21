efbc2145ff17311e960190c4e137fa9f5113d1e5
João Vitor dos Santos <57101445+joaovitorsts@users.noreply.github.com>
2020-04-21 18:03:35 -0300

    Add files via upload

---
+++ lista4exe2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a quantidade números para verificar");
+            int qtdValores = int.Parse(Console.ReadLine());
+            int dentro = 0;
+            int fora = 0;
+           
+            for(int i = 0; i <= qtdValores; i++)
+            {
+                Console.WriteLine("Digite um valor a ser verificado");
+                int valor = int.Parse(Console.ReadLine());
+                if (valor > 9 & valor < 21) 
+                {
+                    dentro += 1;
+                }
+                else
+                {
+                    fora += 1;
+                }
+            }
+
+            Console.WriteLine($"{dentro} in");
+            Console.WriteLine($"{fora} out");
+        }
+    }
+}
